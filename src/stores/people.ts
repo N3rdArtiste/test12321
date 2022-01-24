a6d3482ad708e07219161847926968687fbdc3b6
Raymond Tsui <raymond.tsui@cucumber.co.nz>
2022-01-24 15:28:02 +1300

    removed comments

--- src/stores/people.ts
+++ src/stores/people.ts
@@ -6,24 +6,6 @@ import { term } from 'stores/search'
 
 export const people: AllPeopleQueryStore = operationStore(AllPeopleDocument)
 
-// interface OperationStore extends Readable<OperationStore>
-// export const people: OperationStore<any, object, any> = operationStore(`
-//     query {
-//         allPeople {
-//             people {
-//                 name
-//                 homeworld {
-//                     name
-//                 }
-//                 species {
-//                     name
-//                 }
-//                 gender
-//                 id
-//             }
-//         }
-//     }`)
-
 export const filteredPeople = derived([term, people], ([$term, $people]) => {
     return $people?.data?.allPeople?.people?.filter(x =>
         $term.length > 0
