9bdfa42252dde1a4105f028b438cc59f7a96e445
Robin Singh <robin.singh@cucumber.co.nz>
2022-02-15 12:07:49 +1300

    enabled caching

--- src/_config/graphql-client/client.ts
+++ src/_config/graphql-client/client.ts
@@ -14,12 +14,12 @@ export const client = createClient({
                     judges_by_id: (parent, args) => ({ __typename: 'judges', id: args.id }),
                 },
             },
-            // storage: browser
-            //     ? makeDefaultStorage({
-            //           idbName: 'graphcache-v1', // The name of the IndexedDB database
-            //           maxAge: 100, // The maximum age of the persisted data in days
-            //       })
-            //     : undefined,
+            storage: browser
+                ? makeDefaultStorage({
+                      idbName: 'graphcache-v1', // The name of the IndexedDB database
+                      maxAge: 100, // The maximum age of the persisted data in days
+                  })
+                : undefined,
         }),
         fetchExchange,
     ],
