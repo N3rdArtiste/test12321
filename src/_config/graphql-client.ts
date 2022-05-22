13cdde32195c2f9de0479d78a0e11b6591d3640c
Robin Singh <robin.singh@cucumber.co.nz>
2022-05-22 14:36:09 +1200

    experiment

--- src/_config/graphql-client.ts
+++ src/_config/graphql-client.ts
@@ -7,6 +7,7 @@ import { requestPolicyExchange } from '@urql/exchange-request-policy'
 export const client = (directusURL: string) =>
     createClient({
         url: directusURL + '/graphql',
+        requestPolicy: 'cache-only',
         exchanges: [
             dedupExchange,
             requestPolicyExchange({}),
